543ca343f070936b9fa66ecce585a4a4418caf76
Zahid Wakeel <zhdwkl@gmail.com>
2015-11-09 19:54:32 +0530

    Switched to p2 from arcade physics

--- js/Game.js
+++ js/Game.js
@@ -33,53 +33,67 @@ BasicGame.Game.prototype = {
 
     create: function () {
 
-        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.game.physics.startSystem(Phaser.Physics.P2JS);
+        this.game.physics.p2.setImpactEvents(true);
         this.stage.backgroundColor = '#999999';
 
+        this.game.physics.p2.gravity.y = 500;
+        //this.game.physics.p2.world.defaultContactMaterial.friction = 0.3;
+        //this.game.physics.p2.world.setGlobalStiffness(1e5);
+        //this.game.physics.p2.restitution = 1;
+
         // Ground ledge on which the player will run on it.
-        this.groundLedge = this.game.add.sprite(0, this.game.world.height - 30, 'groundLedge');
-        this.game.physics.arcade.enable(this.groundLedge);
-        this.groundLedge.body.immovable = true;
+        //this.groundLedge = this.game.add.sprite(0, this.game.world.height - 30, 'groundLedge');
+        //this.game.physics.p2.enable(this.groundLedge);
+        //this.groundLedge.anchor.setTo(0, 0);
+        //this.groundLedge.body.kinematic = true;
+        //this.groundLedge.body.static = true;
 
         // Net chord settings...
-        this.net = this.game.add.sprite((this.game.world.width / 2) - 5, this.game.world.height - 230, 'net');
-        this.game.physics.arcade.enable(this.net);
-        this.net.body.immovable = true;
+
+        this.net = this.game.add.sprite((this.game.world.width / 2) - 5, this.game.world.height - 115, 'net');
+        this.game.physics.p2.enable(this.net);
+        this.net.body.static = true;
+
+        // Collisions...
+
+        //var playerCollisionGroup = this.game.physics.p2.createCollisionGroup();
+        //var ballCollisionGroup = this.game.physics.p2.createCollisionGroup();
+
+        //this.game.physics.p2.updateBoundsCollisionGroup();
 
         // Player1 attributes...
 
-        this.player1 = this.game.add.sprite(100, this.game.world.height - 100, 'player');
-        this.game.physics.arcade.enable(this.player1);
-        this.player1.body.bounce.y = BOUNCE;
-        this.player1.body.gravity.y = MAX_GRAVITY;
-        this.player1.body.maxVelocity.setTo(MAX_SPEED_X, MAX_SPEED_Y);
-        this.player1.body.drag.setTo(DRAG, 0);
-        this.player1.body.collideWorldBounds = true;
-
+        this.player1 = this.game.add.sprite(100, this.game.world.height - 78, 'player');
+        this.game.physics.p2.enable(this.player1);
+        this.player1.body.fixedRotation = true;
+        this.player1.body.setCircle(24);
         this.player1.animations.add('left', [0, 1, 2, 3], 10, true);
         this.player1.animations.add('right', [5, 6, 7, 8], 10, true);
+        //this.player1.body.setCollisionGroup(this.playerCollisionGroup);
+        //this.player1.body.collides(this.ballCollisionGroup, this.hitBall, this);
+
+        //this.player1.body.collideWorldBounds = true;
 
         // Player2 attributes...
 
-        this.player2 = this.game.add.sprite(1000, this.game.world.height - 100, 'player');
-        this.game.physics.arcade.enable(this.player2);
-        this.player2.body.bounce.y = BOUNCE;
-        this.player2.body.gravity.y = MAX_GRAVITY;
-        this.player2.body.maxVelocity.setTo(MAX_SPEED_X, MAX_SPEED_Y);
-        this.player2.body.drag.setTo(DRAG, 0);
-        this.player2.body.collideWorldBounds = true;
-
+        this.player2 = this.game.add.sprite(this.game.world.width - 100, this.game.world.height - 115, 'player');
+        this.game.physics.p2.enable(this.player2);
+        this.player2.body.fixedRotation = true;
         this.player2.animations.add('left', [0, 1, 2, 3], 10, true);
         this.player2.animations.add('right', [5, 6, 7, 8], 10, true);
 
         // Ball settings...
 
         this.ball = this.game.add.sprite(200, 200, 'ball');
-        //this.ball.scale.setTo(0.5, 0.5);
-        this.game.physics.arcade.enable(this.ball);
-        this.ball.body.gravity.y = MAX_GRAVITY;
-        this.ball.body.bounce.y = 1;
+        //this.ball.anchor.setTo(0, 0);
+        this.game.physics.p2.enable(this.ball);
+        this.ball.body.setCircle(16);
+        //this.ball.smoothed = true;
+        this.ball.body.restitution = 0.8;
         this.ball.body.collideWorldBounds = true;
+        //this.ball.body.setCollisionGroup(this.ballCollisionGroup);
+        //this.ball.body.collides([this.ballCollisionGroup, this.playerCollisionGroup]);
 
         // Player1 control settings...
 
@@ -93,86 +107,75 @@ BasicGame.Game.prototype = {
 
     },
 
+    hitBall: function (body1, body2) {
+
+    },
+
     update: function () {
-
-        // Handling collisions here...
-
-        this.game.physics.arcade.collide(this.player1, this.groundLedge);
-        this.game.physics.arcade.collide(this.player1, this.net);
-        this.game.physics.arcade.collide(this.player1, this.ball);
-
-        this.game.physics.arcade.collide(this.player2, this.groundLedge);
-        this.game.physics.arcade.collide(this.player2, this.net);
-        this.game.physics.arcade.collide(this.player2, this.ball);
-
-        this.game.physics.arcade.collide(this.ball, this.groundLedge);
-        this.game.physics.arcade.collide(this.ball, this.net);
 
         // Player1 control handling...
 
         if (this.leftButton.isDown) {
             this.player1.animations.play('left');
-            this.player1.body.acceleration.x = -ACCELERATION;
+
+            this.player1.body.velocity.x = -300;
+            //this.player1.body.thrust(-ACCELERATION);
 
         } else if (this.rightButton.isDown) {
             this.player1.animations.play('right');
-            this.player1.body.acceleration.x = ACCELERATION;
+
+            this.player1.body.velocity.x = 300;
+            //this.player1.body.thrust(ACCELERATION);
 
         } else {
             this.player1.animations.stop();
             this.player1.frame = 4;
-            this.player1.body.acceleration.x = 0;
-            //this.player.body.velocity.x = 0;
+            //this.player1.body.acceleration.x = 0;
+            this.player1.body.velocity.x = 0;
         }
-
-        if (this.upButton.isDown && this.player1.body.touching.down) {
+        if (this.upButton.isDown) {
             this.player1.body.velocity.y = -MAX_SPEED_Y;
-
-
         }
+
+        /*if (this.upButton.isDown && this.player1.body.touching.down) {
+            //this.player1.body.velocity.y = -MAX_SPEED_Y;
+            this.player1.body.moveUp(300);
+
+
+        }*/
 
         // Controls for sprite when in the air
 
-        if (!this.player1.body.touching.down) {
+        /*if (!this.player1.body.touching.down) {
             if (this.leftButton.isDown) {
                 this.player1.frame = 3;
             } else if (this.rightButton.isDown) {
                 this.player1.frame = 6;
             }
-        }
+        }*/
 
         // Player2 control handling...
-
         if (this.cursors.left.isDown) {
             this.player2.animations.play('left');
-            this.player2.body.acceleration.x = -ACCELERATION;
+
+            this.player2.body.velocity.x = -300;
+            //this.player1.body.thrust(-ACCELERATION);
 
         } else if (this.cursors.right.isDown) {
             this.player2.animations.play('right');
-            this.player2.body.acceleration.x = ACCELERATION;
+
+            this.player2.body.velocity.x = 300;
+            //this.player1.body.thrust(ACCELERATION);
 
         } else {
             this.player2.animations.stop();
             this.player2.frame = 4;
-            this.player2.body.acceleration.x = 0;
+            //this.player1.body.acceleration.x = 0;
+            this.player2.body.velocity.x = 0;
         }
-
-        if (this.cursors.up.isDown && this.player2.body.touching.down) {
+        if (this.cursors.up.isDown) {
             this.player2.body.velocity.y = -MAX_SPEED_Y;
-
-
         }
-
-        // Controls for sprite when in the air
-
-        if (!this.player2.body.touching.down) {
-            if (this.cursors.left.isDown) {
-                this.player2.frame = 3;
-            } else if (this.cursors.right.isDown) {
-                this.player2.frame = 6;
-            }
-        }
-
-
     }
+
 };
