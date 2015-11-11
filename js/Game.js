054f30a5e6e643d346d19b9b31c95566ad819647
Zahid Wakeel <zhdwkl@gmail.com>
2015-11-11 22:38:13 +0530

    All types of collisions handling done

--- js/Game.js
+++ js/Game.js
@@ -1,229 +1,310 @@
-// Global variables declared here...
+ /* 
+  **********************************
+  * Global variables declared here *
+  **********************************
+  */
 
-var player1;
-var player2;
-var cursors;
-var groundLedge;
-var background;
-var net;
-var ball;
-var jump;
-var playerCollisionGroup;
-var groundCollisionGroup;
-var ballCollisionGroup;
-var netCollisionGroup;
+ var player1;
+ var player2;
+ var cursors;
+ var groundLedge;
+ var background;
+ var net;
+ var ball;
 
-var MAX_SPEED_X = 200;
-var MAX_SPEED_Y = 500;
-var ACCELERATION = 1500;
-var MAX_GRAVITY = 1200;
+ var playerCollisionGroup;
+ var groundCollisionGroup;
+ var ballCollisionGroup;
+ var netCollisionGroup;
 
-var upButton;
-var downButton;
-var leftButton;
-var rightButton;
+ var ballMaterial;
+ var groundMaterial;
+ var playerMaterial;
+ var netMaterial;
+ var worldMaterial;
+ var ballGroundContactMaterial;
+ var ballPlayerContactMaterial;
+ var ballWorldContactMaterial;
+ var ballNetContactMaterial;
 
-BasicGame.Game = function (game) {
+ var MAX_SPEED_X = 200;
+ var MAX_SPEED_Y = 500;
+ var ACCELERATION = 1500;
+ var MAX_GRAVITY = 1200;
 
-};
+ var upButton;
+ var downButton;
+ var leftButton;
+ var rightButton;
 
-BasicGame.Game.prototype = {
+ BasicGame.Game = function (game) {
 
-    preload: function () {
+ };
 
-    },
+ BasicGame.Game.prototype = {
 
-    create: function () {
+ 	preload: function () {
+ 		// All assets loaded already...
+ 	},
 
-        this.game.physics.startSystem(Phaser.Physics.P2JS);
-        this.game.physics.p2.setImpactEvents(true);
-        this.stage.backgroundColor = '#999999';
+ 	create: function () {
 
-        this.game.physics.p2.gravity.y = 1000;
-        //this.game.physics.p2.world.defaultContactMaterial.friction = 0.3;
-        //this.game.physics.p2.world.setGlobalStiffness(1e5);
-        //this.game.physics.p2.restitution = 0.7;
-        //this.ballMaterial = thisgame.physics.p2.createMaterial('ballMaterial', this.ball.body);
-        //this.groundMaterial = this.game.physics.p2.createMaterial('groundMaterial', this.groundLedge.body);
+ 		this.game.physics.startSystem(Phaser.Physics.P2JS);
+ 		this.game.physics.p2.setImpactEvents(true);
+ 		this.stage.backgroundColor = '#999999';
+ 		this.game.physics.p2.gravity.y = 800;
 
-        // Collisions...
+ 		// Collisions...
 
-        this.playerCollisionGroup = this.game.physics.p2.createCollisionGroup();
-        this.ballCollisionGroup = this.game.physics.p2.createCollisionGroup();
-        this.groundCollisionGroup = this.game.physics.p2.createCollisionGroup();
-        this.netCollisionGroup = this.game.physics.p2.createCollisionGroup();
-        this.game.physics.p2.updateBoundsCollisionGroup();
+ 		this.playerCollisionGroup = this.game.physics.p2.createCollisionGroup();
+ 		this.ballCollisionGroup = this.game.physics.p2.createCollisionGroup();
+ 		this.groundCollisionGroup = this.game.physics.p2.createCollisionGroup();
+ 		this.netCollisionGroup = this.game.physics.p2.createCollisionGroup();
+ 		this.game.physics.p2.updateBoundsCollisionGroup();
 
-        // Ground ledge on which the player will run on it.
-        this.groundLedge = this.game.add.sprite(this.game.world.width / 2, this.game.world.height, 'groundLedge');
-        this.game.physics.p2.enable(this.groundLedge);
-        this.groundLedge.body.kinematic = true;
-        //this.groundLedge.body.fixedRotation = true;
-        this.groundLedge.body.setCollisionGroup(this.groundCollisionGroup);
-        this.groundLedge.body.collides(this.playerCollisionGroup, this.playerGroundCollision, this);
-        this.groundLedge.body.collides(this.ballCollisionGroup, this.ballgroundCollision, this);
+ 		/* 
+ 		 ***************************************************
+ 		 * Ground Ledge on which the player will run on it *
+ 		 ***************************************************
+ 		 */
 
-        // Net chord settings...
+ 		this.groundLedge = this.game.add.sprite(this.game.world.width / 2, this.game.world.height, 'groundLedge');
+ 		this.game.physics.p2.enable(this.groundLedge);
+ 		this.groundLedge.body.kinematic = true;
+ 		this.groundLedge.body.setCollisionGroup(this.groundCollisionGroup);
+ 		this.groundLedge.body.collides(this.playerCollisionGroup, this.playerGroundCollision, this);
+ 		this.groundLedge.body.collides(this.ballCollisionGroup, this.ballgroundCollision, this);
 
-        this.net = this.game.add.sprite((this.game.world.width / 2) - 5, this.game.world.height - 115, 'net');
-        this.game.physics.p2.enable(this.net);
-        this.net.body.static = true;
-        this.net.body.setCollisionGroup(this.netCollisionGroup);
-        this.net.body.collides(this.ballCollisionGroup, this.netBallCollision, this);
-        this.net.body.collides(this.playerCollisionGroup, this.playerNetCollision, this);
+ 		/* 
+ 		 ***************************
+ 		 * Net chord settings here *
+ 		 ***************************
+ 		 */
 
-        // Create two players...
+ 		this.net = this.game.add.sprite((this.game.world.width / 2) - 5, this.game.world.height - 115, 'net');
+ 		this.game.physics.p2.enable(this.net);
+ 		this.net.body.static = true;
+ 		this.net.body.setCollisionGroup(this.netCollisionGroup);
+ 		this.net.body.collides(this.ballCollisionGroup, this.netBallCollision, this);
+ 		this.net.body.collides(this.playerCollisionGroup, this.playerNetCollision, this);
 
-        this.player1 = this.createPlayer(this.player1, 100, this.game.world.height - 78);
-        this.player2 = this.createPlayer(this.player2, this.game.world.width - 100, this.game.world.height - 115);
+ 		// Create two players...
 
-        // Ball settings...
+ 		this.player1 = this.createPlayer(this.player1, 100, this.game.world.height - 78);
+ 		this.player2 = this.createPlayer(this.player2, this.game.world.width - 100, this.game.world.height - 115);
 
-        this.ball = this.game.add.sprite(200, 200, 'ball');
-        //this.ball.anchor.setTo(0, 0);
-        this.game.physics.p2.enable(this.ball);
-        this.ball.body.setCircle(16);
-        //this.ball.smoothed = true;
-        this.ball.body.collideWorldBounds = true;
-        this.ball.body.setCollisionGroup(this.ballCollisionGroup);
-        this.ball.body.collides(this.groundCollisionGroup, this.ballGroundCollision, this);
-        this.ball.body.collides(this.playerCollisionGroup, this.playerBallCollision, this);
-        this.ball.body.collides(this.netCollisionGroup, this.netBallCollision, this);
+ 		/* 
+ 		 ************************
+ 		 * Ball properties here *
+ 		 ************************
+ 		 */
 
-        // Player1 control settings...
+ 		this.ball = this.game.add.sprite(200, 200, 'ball');
+ 		this.game.physics.p2.enable(this.ball);
+ 		this.ball.body.setCircle(16);
+ 		this.ball.smoothed = true;
+ 		this.ball.body.collideWorldBounds = true;
+ 		this.ball.body.setCollisionGroup(this.ballCollisionGroup);
+ 		this.ball.body.collides(this.groundCollisionGroup, this.ballGroundCollision, this);
+ 		this.ball.body.collides(this.playerCollisionGroup, this.playerBallCollision, this);
+ 		this.ball.body.collides(this.netCollisionGroup, this.netBallCollision, this);
 
-        this.upButton = this.game.input.keyboard.addKey(Phaser.Keyboard.W);
-        this.downButton = this.game.input.keyboard.addKey(Phaser.Keyboard.S);
-        this.leftButton = this.game.input.keyboard.addKey(Phaser.Keyboard.A);
-        this.rightButton = this.game.input.keyboard.addKey(Phaser.Keyboard.D);
+ 		/* 
+ 		 **********************
+ 		 * Contact Materials  *
+ 		 **********************
+ 		 */
 
-        // Player2 control settings...
-        this.cursors = this.game.input.keyboard.createCursorKeys();
+ 		this.ballMaterial = this.game.physics.p2.createMaterial('ballMaterial');
+ 		this.groundMaterial = this.game.physics.p2.createMaterial('groundMaterial');
+ 		this.worldMaterial = this.game.physics.p2.createMaterial('worldMaterial');
+ 		this.playerMaterial = this.game.physics.p2.createMaterial('playerMaterial');
+ 		this.netMaterial = this.game.physics.p2.createMaterial('netMaterial');
 
-    },
+ 		this.ball.body.setMaterial(this.ballMaterial);
+ 		this.groundLedge.body.setMaterial(this.groundMaterial);
+ 		this.player1.body.setMaterial(this.playerMaterial);
+ 		this.player2.body.setMaterial(this.playerMaterial);
+ 		this.net.body.setMaterial(this.netMaterial);
+ 		this.game.physics.p2.setWorldMaterial(this.worldMaterial, true, true, true, true);
 
+ 		// Ball/World collision settings here...
 
-    createPlayer: function (player, x, y) {
+ 		this.ballWorldContactMaterial = this.game.physics.p2.createContactMaterial(this.ballMaterial, this.worldMaterial);
+ 		this.ballWorldContactMaterial.restitution = 0.8;
 
-        this.player = this.game.add.sprite(x, y, 'player');
-        this.game.physics.p2.enable(this.player);
-        this.player.body.fixedRotation = true;
-        this.player.body.setCircle(24);
+ 		// Ball/Ground collision settings here...
 
-        this.player.animations.add('left', [0, 1, 2, 3], 10, true);
-        this.player.animations.add('right', [5, 6, 7, 8], 10, true);
+ 		this.ballGroundContactMaterial = this.game.physics.p2.createContactMaterial(this.ballMaterial, this.groundMaterial);
+ 		this.ballGroundContactMaterial.restitution = 0.8;
 
-        this.player.body.setCollisionGroup(this.playerCollisionGroup);
-        this.player.body.collides(this.groundCollisionGroup, this.playerGroundCollision, this);
-        this.player.body.collides(this.ballCollisionGroup, this.playerBallCollision, this);
-        this.player.body.collides(this.netCollisionGroup, this.playerNetCollision, this);
+ 		// Ball/Player collision settings here...
 
-        this.player.body.collideWorldBounds = true;
-        return this.player;
-    },
+ 		this.ballPlayerContactMaterial = this.game.physics.p2.createContactMaterial(this.ballMaterial, this.playerMaterial);
+ 		this.ballPlayerContactMaterial.restitution = 0.8;
 
-    playerGroundCollision: function (body1, body2) {
-        console.log("Player/Ground Collision!");
-        //this.jump = true;
-    },
+ 		// Ball/Net collision settings here...
 
-    ballGroundCollision: function (body1, body2) {
-        console.log("Ball/Ground Collision!");
-    },
+ 		this.ballNetContactMaterial = this.game.physics.p2.createContactMaterial(this.ballMaterial, this.netMaterial);
+ 		this.ballNetContactMaterial.restitution = 0.8;
 
-    playerBallCollision: function (body1, body2) {
-        console.log("Player/Ball Collision!");
-    },
+ 		/* 
+ 		 **********************************
+ 		 * Player1 control settings here *
+ 		 **********************************
+ 		 */
 
-    netBallCollision: function (body1, body2) {
-        console.log("Net/Ball Collision!");
-    },
+ 		this.upButton = this.game.input.keyboard.addKey(Phaser.Keyboard.W);
+ 		this.downButton = this.game.input.keyboard.addKey(Phaser.Keyboard.S);
+ 		this.leftButton = this.game.input.keyboard.addKey(Phaser.Keyboard.A);
+ 		this.rightButton = this.game.input.keyboard.addKey(Phaser.Keyboard.D);
 
-    playerNetCollision: function (body1, body2) {
-        console.log("Player/Net Collision");
-    },
+ 		// Player2 control settings...
 
-    touchingDown: function (someone) {
-        var yAxis = p2.vec2.fromValues(0, 1);
-        var result = false;
-        for (var i = 0; i < game.physics.p2.world.narrowphase.contactEquations.length; i++) {
-            var c = game.physics.p2.world.narrowphase.contactEquations[i]; // cycles through all the contactEquations until it finds our "someone"
-            if (c.bodyA === someone.body.data || c.bodyB === someone.body.data) {
-                var d = p2.vec2.dot(c.normalA, yAxis); // Normal dot Y-axis
-                if (c.bodyA === someone.body.data) d *= -1;
-                if (d > 0.5) result = true;
-            }
-        }
-        return result;
-    },
+ 		this.cursors = this.game.input.keyboard.createCursorKeys();
 
-    update: function () {
+ 	},
 
-        // Player1 control handling...
+ 	/* 
+ 	 *****************************
+ 	 * Function to create player *
+ 	 *****************************
+ 	 */
 
-        if (this.leftButton.isDown) {
-            this.player1.animations.play('left');
+ 	createPlayer: function (player, x, y) {
 
-            this.player1.body.velocity.x = -300;
-            //this.player1.body.thrust(-ACCELERATION);
+ 		this.player = this.game.add.sprite(x, y, 'player');
+ 		this.game.physics.p2.enable(this.player);
+ 		this.player.body.fixedRotation = true;
+ 		this.player.body.setCircle(24);
 
-        } else if (this.rightButton.isDown) {
-            this.player1.animations.play('right');
+ 		this.player.animations.add('left', [0, 1, 2, 3], 10, true);
+ 		this.player.animations.add('right', [5, 6, 7, 8], 10, true);
 
-            this.player1.body.velocity.x = 300;
-            //this.player1.body.thrust(ACCELERATION);
+ 		this.player.body.setCollisionGroup(this.playerCollisionGroup);
+ 		this.player.body.collides(this.groundCollisionGroup, this.playerGroundCollision, this);
+ 		this.player.body.collides(this.ballCollisionGroup, this.playerBallCollision, this);
+ 		this.player.body.collides(this.netCollisionGroup, this.playerNetCollision, this);
 
-        } else {
-            this.player1.animations.stop();
-            this.player1.frame = 4;
-            //this.player1.body.acceleration.x = 0;
-            this.player1.body.velocity.x = 0;
-        }
-        if (this.upButton.isDown && this.touchingDown(this.player1)) {
-            this.player1.body.velocity.y = -MAX_SPEED_Y;
-        }
+ 		this.player.body.collideWorldBounds = true;
+ 		return this.player;
+ 	},
 
-        // Controls for sprite when in the air
+ 	/* 
+ 	 ********************************
+ 	 * All Collisions handling here *
+ 	 ********************************
+ 	 */
 
-        if (!this.touchingDown(this.player1)) {
-            if (this.leftButton.isDown) {
-                this.player1.frame = 3;
-            } else if (this.rightButton.isDown) {
-                this.player1.frame = 6;
-            }
-        }
+ 	playerGroundCollision: function (body1, body2) {
+ 		console.log("Player/Ground Collision!");
+ 	},
 
-        // Player2 control handling...
-        if (this.cursors.left.isDown) {
-            this.player2.animations.play('left');
+ 	ballGroundCollision: function (body1, body2) {
+ 		console.log("Ball/Ground Collision!");
+ 	},
 
-            this.player2.body.velocity.x = -300;
-            //this.player1.body.thrust(-ACCELERATION);
+ 	playerBallCollision: function (body1, body2) {
+ 		console.log("Player/Ball Collision!");
+ 		this.ball.body.velocity.y = -700;
+ 	},
 
-        } else if (this.cursors.right.isDown) {
-            this.player2.animations.play('right');
+ 	netBallCollision: function (body1, body2) {
+ 		console.log("Net/Ball Collision!");
+ 	},
 
-            this.player2.body.velocity.x = 300;
-            //this.player1.body.thrust(ACCELERATION);
+ 	playerNetCollision: function (body1, body2) {
+ 		console.log("Player/Net Collision");
+ 	},
 
-        } else {
-            this.player2.animations.stop();
-            this.player2.frame = 4;
-            this.player2.body.velocity.x = 0;
-        }
+ 	/* 
+ 	 ******************************************************
+ 	 * Checking whether a player touches the ground or not. *
+ 	 ******************************************************
+ 	 */
 
-        if (this.cursors.up.isDown && this.touchingDown(this.player2)) {
-            this.player2.body.velocity.y = -MAX_SPEED_Y;
-        }
+ 	touchingDown: function (someone) {
+ 		var yAxis = p2.vec2.fromValues(0, 1);
+ 		var result = false;
+ 		for (var i = 0; i < game.physics.p2.world.narrowphase.contactEquations.length; i++) {
+ 			var c = game.physics.p2.world.narrowphase.contactEquations[i]; // cycles through all the contactEquations until it finds our "someone"
+ 			if (c.bodyA === someone.body.data || c.bodyB === someone.body.data) {
+ 				var d = p2.vec2.dot(c.normalA, yAxis); // Normal dot Y-axis
+ 				if (c.bodyA === someone.body.data) d *= -1;
+ 				if (d > 0.5) result = true;
+ 			}
+ 		}
+ 		return result;
+ 	},
 
-        if (!this.touchingDown(this.player2)) {
-            if (this.cursors.left.isDown) {
-                this.player2.frame = 3;
-            } else if (this.cursors.right.isDown) {
-                this.player2.frame = 6;
-            }
-        }
+ 	update: function () {
 
-    }
+ 		/* 
+ 		 *******************************
+ 		 * Player1 control handling... *
+ 		 *******************************
+ 		 */
 
-};
+ 		if (this.leftButton.isDown) {
+ 			this.player1.animations.play('left');
+ 			this.player1.body.velocity.x = -300;
+ 		} else if (this.rightButton.isDown) {
+ 			this.player1.animations.play('right');
+ 			this.player1.body.velocity.x = 300;
+ 		} else {
+ 			this.player1.animations.stop();
+ 			this.player1.frame = 4;
+ 			this.player1.body.velocity.x = 0;
+ 		}
+
+ 		if (this.upButton.isDown && this.touchingDown(this.player1)) {
+ 			this.player1.body.velocity.y = -MAX_SPEED_Y;
+ 		}
+
+ 		// Controls for player1 when in the air
+
+ 		if (!this.touchingDown(this.player1)) {
+ 			if (this.leftButton.isDown) {
+ 				this.player1.frame = 3;
+ 			} else if (this.rightButton.isDown) {
+ 				this.player1.frame = 6;
+ 			}
+ 		}
+
+ 		/* 
+ 		 *******************************
+ 		 * Player2 control handling... *
+ 		 *******************************
+ 		 */
+
+ 		if (this.cursors.left.isDown) {
+ 			this.player2.animations.play('left');
+ 			this.player2.body.velocity.x = -300;
+
+ 		} else if (this.cursors.right.isDown) {
+ 			this.player2.animations.play('right');
+ 			this.player2.body.velocity.x = 300;
+
+ 		} else {
+ 			this.player2.animations.stop();
+ 			this.player2.frame = 4;
+ 			this.player2.body.velocity.x = 0;
+ 		}
+
+ 		if (this.cursors.up.isDown && this.touchingDown(this.player2)) {
+ 			this.player2.body.velocity.y = -MAX_SPEED_Y;
+ 		}
+
+ 		// Controls for player2 when in the air
+
+ 		if (!this.touchingDown(this.player2)) {
+ 			if (this.cursors.left.isDown) {
+ 				this.player2.frame = 3;
+ 			} else if (this.cursors.right.isDown) {
+ 				this.player2.frame = 6;
+ 			}
+ 		}
+
+ 	}
+
+ };
